0af656c4ee68fba8712d4f9872879c98a32cb84f
Hasantha32 <hasanthamadushan32@gmail.com>
2020-09-29 10:00:40 +0530

    Initial commit of Android project

---
+++ app/src/main/java/com/example/lizard/DialogBox.java
@@ -0,0 +1,24 @@
+package com.example.lizard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class DialogBox extends AppCompatActivity {
+
+    private static final String TAG = "DialogBox";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_box);
+
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/lizard/showImages.java
@@ -0,0 +1,60 @@
+package com.example.lizard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class showImages extends AppCompatActivity implements RecyclerViewClickInterface{
+
+    private DatabaseHandler objectDatabaseHandler;
+    private RecyclerView objectRecyclerview;
+    private RVAdapter objectRVAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_images);
+
+        try {
+            objectRecyclerview = findViewById(R.id.imagesRV);
+            objectDatabaseHandler = new DatabaseHandler(this);
+        }catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void getData(View view){
+        try {
+            objectRVAdapter = new RVAdapter(objectDatabaseHandler.getImagesData(),this);
+            objectRecyclerview.setHasFixedSize(true);
+
+            objectRecyclerview.setLayoutManager(new LinearLayoutManager(this));
+            objectRecyclerview.setAdapter(objectRVAdapter);
+
+        }catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    public void onItemClick(int position) {
+        Toast.makeText(this, "Long Press to delete selected item...", Toast.LENGTH_SHORT).show();
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onLongItemClick(int position) {
+     //   objectRVAdapter.remove(position);
+    }
+}
